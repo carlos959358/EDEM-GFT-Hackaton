47ce1fd28284d80205a966ad4d32ecdabc5e9576
cabesaEDEM <cabesa@edem.es>
2026-03-25 16:31:58 +0100

    terraform state bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "gcs" {
+    bucket      = "gft-hackaton-tfstate"
+    prefix      = "terraform/state"
+    credentials = "../spa-datajuniorsprogram-sdb-001-899009cc32ac.json"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
