928837cefcbe1a90551d20b4e090e153e353c1ea
cabesaEDEM <cabesa@edem.es>
2026-03-25 18:11:29 +0100

    base cicd and frontend

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,8 @@ terraform {
   required_version = ">= 1.0"
 
   backend "gcs" {
-    bucket      = "gft-hackaton-tfstate"
-    prefix      = "terraform/state"
-    credentials = "../spa-datajuniorsprogram-sdb-001-899009cc32ac.json"
+    bucket = "gft-hackaton-tfstate-26"
+    prefix = "terraform/state"
   }
 
   required_providers {
@@ -16,50 +15,6 @@ terraform {
 }
 
 provider "google" {
-  project     = var.project_id
-  region      = var.region
-  credentials = file(var.credentials_file)
-}
-
-#Creamos la cuenta de servicio de cloud run
-resource "google_service_account" "cloud_run_sa" {
-  account_id   = "hackatone-cloud-run-sa"
-  display_name = "Service Account para ${var.service_name}"
-}
-
-resource "google_cloud_run_v2_service" "default" {
-  name     = var.service_name
-  location = var.region
-  ingress  = "INGRESS_TRAFFIC_ALL"
-
-  template {
-    service_account = google_service_account.cloud_run_sa.email
-
-    scaling {
-      min_instance_count = 0 
-      max_instance_count = 1
-    }
-
-    containers {
-      image = var.container_image
-      
-      resources {
-        limits = {
-          cpu    = "1"
-          memory = "512Mi"
-        }
-      }
-      
-      ports {
-        container_port = 8080
-      }
-    }
-  }
-}
-
-resource "google_cloud_run_v2_service_iam_member" "public_access" {
-  name     = google_cloud_run_v2_service.default.name
-  location = google_cloud_run_v2_service.default.location
-  role     = "roles/run.invoker"
-  member   = "allUsers"
+  project = var.project_id
+  region  = var.region
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "project_id" {
   description = "GCP project ID"
   type        = string
-  default     = "spa-datajuniorsprogram-sdb-001"
+  default     = "edem-hackathon-2026"
 }
 
 variable "region" {
@@ -10,21 +10,5 @@ variable "region" {
   default     = "europe-west1"
 }
 
-variable "credentials_file" {
-  description = "Path to the GCP service account key file"
-  type        = string
-  default     = "../spa-datajuniorsprogram-sdb-001-899009cc32ac.json"
-}
 
-variable "service_name" {
-  description = "Nombre del recurso en Cloud Run"
-  type        = string
-  default     = "cloudrun-service"
-}
-
-variable "container_image" {
-  description = "La imagen de Docker que se va a desplegar"
-  type        = string
-  default     = "us-docker.pkg.dev/cloudrun/container/hello" #cambiar cuando sea necesario
-}
 
