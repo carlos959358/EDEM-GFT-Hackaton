0b1f3e0c17913255f95f8fa200bcc0e4cd307600
gemmabalagueer <gbalagueradell@gmail.com>
2026-03-25 17:19:35 +0100

    Firestore

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,45 +21,12 @@ provider "google" {
   credentials = file(var.credentials_file)
 }
 
-#Creamos la cuenta de servicio de cloud run
-resource "google_service_account" "cloud_run_sa" {
-  account_id   = "hackatone-cloud-run-sa"
-  display_name = "Service Account para ${var.service_name}"
-}
+# Crear la base de datos Firestore (Modo Nativo)
+resource "google_firestore_database" "database" {
+  project     = var.project_id
+  name        = "(default)" # Es el nombre estándar requerido
+  location_id = var.firestore_location
+  type        = "FIRESTORE_NATIVE"
 
-resource "google_cloud_run_v2_service" "default" {
-  name     = var.service_name
-  location = var.region
-  ingress  = "INGRESS_TRAFFIC_ALL"
-
-  template {
-    service_account = google_service_account.cloud_run_sa.email
-
-    scaling {
-      min_instance_count = 0 
-      max_instance_count = 1
-    }
-
-    containers {
-      image = var.container_image
-      
-      resources {
-        limits = {
-          cpu    = "1"
-          memory = "512Mi"
-        }
-      }
-      
-      ports {
-        container_port = 8080
-      }
-    }
-  }
-}
-
-resource "google_cloud_run_v2_service_iam_member" "public_access" {
-  name     = google_cloud_run_v2_service.default.name
-  location = google_cloud_run_v2_service.default.location
-  role     = "roles/run.invoker"
-  member   = "allUsers"
+  depends_on = [google_project_service.firestore]
 }
