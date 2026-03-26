3106c2fcedcb26e0f728a9191a7c6c8b9ba4afc9
SalvillaT <salvador.reche.vazquez@gmail.com>
2026-03-26 16:26:37 +0100

    Pagina de pasar asistencia añadida

--- frontend/src/app/routes.ts
+++ frontend/src/app/routes.ts
@@ -11,6 +11,7 @@ import { NotificationsScreen } from './components/NotificationsScreen';
 import { CoursesScreen } from './components/CoursesScreen';
 import { CourseDetailScreen } from './components/CourseDetailScreen';
 import { GradingScreen } from './components/GradingScreen';
+import { ClassAttendanceScreen } from './components/ClassAttendanceScreen';
 import { Layout } from './components/Layout';
 
 export const router = createBrowserRouter([
@@ -67,5 +68,9 @@ export const router = createBrowserRouter([
   {
     path: 'courses/:courseId/subjects/:subjectId/grade',
     Component: GradingScreen,
+  },
+  {
+    path: 'class/:classId/attendance',
+    Component: ClassAttendanceScreen,
   },
 ]);
